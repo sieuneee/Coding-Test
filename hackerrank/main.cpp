45e591634559722312a9995630eee17dea244e0a
sieuneee <kseun021@naver.com>
2021-10-01 15:33:24 +0900

    Initial Commit

---
+++ hackerrank/main.cpp
@@ -0,0 +1,15 @@
+//
+//  main.cpp
+//  hackerrank
+//
+//  Created by user on 2021/10/01.
+//  Copyright © 2021 김시은. All rights reserved.
+//
+
+#include <iostream>
+
+int main(int argc, const char * argv[]) {
+    // insert code here...
+    std::cout << "Hello, World!\n";
+    return 0;
+}
